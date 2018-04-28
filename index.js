8512464b3ba2f2f4af296fb1ceadf34416dc892d
Chris Wilcox <ckwilcox@gmail.com>
2018-04-28 13:30:49 -0500

    more lodash funcs

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _some = require('lodash/some');
-
-var _some2 = _interopRequireDefault(_some);
-
 var _bind = require('lodash/bind');
 
 var _bind2 = _interopRequireDefault(_bind);
@@ -20,6 +16,10 @@ var _clone = require('lodash/clone');
 
 var _clone2 = _interopRequireDefault(_clone);
 
+var _create = require('lodash/create');
+
+var _create2 = _interopRequireDefault(_create);
+
 var _defaults = require('lodash/defaults');
 
 var _defaults2 = _interopRequireDefault(_defaults);
@@ -31,6 +31,10 @@ var _each2 = _interopRequireDefault(_each);
 var _escape2 = require('lodash/escape');
 
 var _escape3 = _interopRequireDefault(_escape2);
+
+var _extend = require('lodash/extend');
+
+var _extend2 = _interopRequireDefault(_extend);
 
 var _has = require('lodash/has');
 
@@ -68,6 +72,10 @@ var _isString = require('lodash/isString');
 
 var _isString2 = _interopRequireDefault(_isString);
 
+var _iteratee = require('lodash/iteratee');
+
+var _iteratee2 = _interopRequireDefault(_iteratee);
+
 var _keys = require('lodash/keys');
 
 var _keys2 = _interopRequireDefault(_keys);
@@ -75,6 +83,10 @@ var _keys2 = _interopRequireDefault(_keys);
 var _map = require('lodash/map');
 
 var _map2 = _interopRequireDefault(_map);
+
+var _matches = require('lodash/matches');
+
+var _matches2 = _interopRequireDefault(_matches);
 
 var _once = require('lodash/once');
 
@@ -88,6 +100,14 @@ var _result = require('lodash/result');
 
 var _result2 = _interopRequireDefault(_result);
 
+var _size = require('lodash/size');
+
+var _size2 = _interopRequireDefault(_size);
+
+var _some = require('lodash/some');
+
+var _some2 = _interopRequireDefault(_some);
+
 var _uniqueId = require('lodash/uniqueId');
 
 var _uniqueId2 = _interopRequireDefault(_uniqueId);
@@ -99,9 +119,11 @@ exports.default = {
   bind: _bind2.default,
   bindAll: _bindAll2.default,
   clone: _clone2.default,
+  create: _create2.default,
   defaults: _defaults2.default,
   each: _each2.default,
   'escape': _escape3.default, // escape is a protected var
+  extend: _extend2.default,
   has: _has2.default,
   invoke: _invoke2.default,
   isArray: _isArray2.default,
@@ -111,11 +133,14 @@ exports.default = {
   isObject: _isObject2.default,
   isRegExp: _isRegExp2.default,
   isString: _isString2.default,
+  iteratee: _iteratee2.default,
   keys: _keys2.default,
   map: _map2.default,
+  matches: _matches2.default,
   once: _once2.default,
   pick: _pick2.default,
   result: _result2.default,
+  size: _size2.default,
   some: _some2.default,
   uniqueId: _uniqueId2.default
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import some from 'lodash/some';
 import bind from 'lodash/bind';
 import bindAll from 'lodash/bindAll';
 import clone from 'lodash/clone';
+import create from 'lodash/create';
 import defaults from 'lodash/defaults';
 import each from 'lodash/each';
 import _escape from 'lodash/escape';
+import extend from 'lodash/extend';
 import has from 'lodash/has';
 import invoke from 'lodash/invoke';
 import isArray from 'lodash/isArray';
@@ -14,11 +15,15 @@ import isFunction from 'lodash/isFunction';
 import isObject from 'lodash/isObject';
 import isRegExp from 'lodash/isRegExp';
 import isString from 'lodash/isString';
+import iteratee from 'lodash/iteratee';
 import keys from 'lodash/keys';
 import map from 'lodash/map';
+import matches from 'lodash/matches';
 import once from 'lodash/once';
 import pick from 'lodash/pick';
 import result from 'lodash/result';
+import size from 'lodash/size';
+import some from 'lodash/some';
 import uniqueId from 'lodash/uniqueId';
 
 
@@ -27,9 +32,11 @@ export default {
   bind,
   bindAll,
   clone,
+  create,
   defaults,
   each,
   'escape': _escape, // escape is a protected var
+  extend,
   has,
   invoke,
   isArray,
@@ -39,11 +46,14 @@ export default {
   isObject,
   isRegExp,
   isString,
+  iteratee,
   keys,
   map,
+  matches,
   once,
   pick,
   result,
+  size,
   some,
   uniqueId
-}
+};
